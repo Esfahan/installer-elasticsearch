539d4e411ad60dce3303ca66c4f8e17d1997abf5
Esfahan <calito0131@gmail.com>
2017-10-12 07:07:13 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# installer-elasticsearch
+Installer for Elasticsearch
+
+## Usage
+
+```
+$ bash setup.sh
+```
+
+## Choose JDK
+If you have other JDK, choose JDK with following command.
+
+```
+$ sudo alternatives --config java
+
+There is 1 program that provides 'java'.
+
+  Selection    Command
+-----------------------------------------------
+   1           java-1.8.0-openjdk.x86_64 (/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.144-0.b01.el7_4.x86_64/jre/bin/java)
+*+ 2           /usr/java/jdk1.8.0_71/jre/bin/java
+
+Enter to keep the current selection[+], or type selection number: 1
+```
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# java
+sudo yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
+
+if [ `cat /etc/profile | grep 'JAVA_HOME' > /dev/null 2>&1; echo $?` == 0 ]; then
+  echo '(skipping...)environment is alreday set.'
+else
+  sudo /bin/bash -lc 'cat java/environment >> /etc/profile'
+  source /etc/profile
+fi
+
+# elasticsearch
+sudo cp repo/elasticsearch.repo /etc/yum.repos.d/elasticsearch.repo
+sudo yum -y install elasticsearch
+sudo systemctl start elasticsearch
+sudo systemctl enable elasticsearch
+
+# to use japanese
+sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-kuromoji
+sudo /usr/share/elasticsearch/bin/elasticsearch-plugin list | grep analysis-kuromoji
+
+curl http://127.0.0.1:9200
